86638cada31949d2036d1ec38935f8b8eb2b7e91
Mathias Betancurt <mathias@betancurt.dev>
2020-05-27 16:53:31 +0200

    docs: Preparing for release

--- src/Ades.js
+++ src/Ades.js
@@ -6,7 +6,6 @@ import React, { useState, useEffect } from 'react';
 //import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import {
-	Alert,
 	Popover,
 	Position,
 	Icon,
