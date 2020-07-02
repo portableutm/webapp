30c44f6fbd1ec9c300c68c18f17e404d98a5c22f
Mathias Betancurt <mathias@betancurt.dev>
2020-07-02 17:41:57 +0200

    feat: pilots can change their personal info and password

--- src/dashboard/user/Pilot.js
+++ src/dashboard/user/Pilot.js
@@ -1,12 +1,72 @@
-import React from 'react';
+import React, {useState} from 'react';
 import profile from '../../images/profile.png';
-import {Card, Elevation, FormGroup, InputGroup} from '@blueprintjs/core';
+import {Button, Intent, Card, Elevation, FormGroup, InputGroup} from '@blueprintjs/core';
 import {useTranslation} from 'react-i18next';
+import useAdesState, {Axios} from '../../state/AdesState';
+import {fM} from '../../libs/SaferSanctuary';
 
 function Pilot({user}) {
 	const { t, } = useTranslation();
 	//const { username } = useParams();
 	//const [ user, setUser ] = useState(passedUser);
+	const [isUserDataChangeEnabled, setUserDataChangeEnabled] = useState(true);
+	const [isPasswordChangeEnabled, setPasswordChangeEnabled] = useState(false);
+	const [isEmailValid, setIsEmailValid] = useState(true);
+	const [state, actions] = useAdesState();
+
+	const onEmailInputChange = (evt) => {
+		const email = evt.currentTarget.value;
+		const hasAtSign = email.includes('@');
+		const hasDotSign = email.includes('.');
+		const hasNoSpace = !email.includes(' ');
+		/* We validate emails using a lax policy here, contrary to the user registration */
+		/* We should actually verify these emails are valid by confirming also their new address */
+		/* not by enforcing a complicated pattern */
+		// TODO: Confirm new user email is valid.
+		setIsEmailValid(hasAtSign && hasDotSign && hasNoSpace);
+	};
+
+	const changePassword = () => {
+		const newUserData = {...user};
+		setPasswordChangeEnabled(false);
+		setUserDataChangeEnabled(false);
+		newUserData.password = document.getElementById('newpassword').value;
+		//if (newUserData.password.includes(' ')) alert('Your password contains an invalid character');
+		Axios
+			.put('/user/password/' + newUserData.username, newUserData, {headers: {auth: fM(state.auth.token)}})
+			.then(() =>
+				actions.auth.info(newUserData.username, () => {
+					alert('Password change was successful');
+					setUserDataChangeEnabled(true);
+				}, () => {
+					actions.auth.logout();
+				})
+			)
+			.catch(() => {
+				actions.auth.logout();
+			});
+	};
+	const changeUserData = () => {
+		setUserDataChangeEnabled(false);
+		const newUserData = {...user};
+		newUserData.firstName = document.getElementById('firstName').value;
+		newUserData.lastName = document.getElementById('lastName').value;
+		newUserData.email = document.getElementById('email').value;
+		Axios
+			.put('/user/info/' + newUserData.username, newUserData, {headers: {auth: fM(state.auth.token)}})
+			.then(() =>
+				actions.auth.info(newUserData.username, () => {
+					alert('Data change was successful');
+					setUserDataChangeEnabled(true);
+				}, () => {
+					actions.auth.logout();
+				})
+			)
+			.catch((error) => {
+				alert('Data change failed: ' + error.response.data[0]);
+				setUserDataChangeEnabled(true);
+			});
+	};
 
 	/*
 	useEffect(() => {
@@ -46,48 +106,81 @@ function Pilot({user}) {
 						<Card className='fullHW' elevation={Elevation.TWO}>
 							<FormGroup
 								label={t('user.username')}
-								labelFor="text-input"
+								labelFor="username"
 								labelInfo="(non-editable)"
 							>
-								<InputGroup leftIcon="person" id="text-input" disabled={true} value={user.username}/>
+								<InputGroup leftIcon="person" disabled={true} id="username"  value={user.username}/>
 							</FormGroup>
 							<FormGroup
 								label={t('user.firstname')}
-								labelFor="text-input"
+								labelFor="firstName"
 							>
-								<InputGroup leftIcon="person" id="text-input" disabled={true} value={user.firstName}/>
+								<InputGroup leftIcon="person" disabled={!isUserDataChangeEnabled} id="firstName" defaultValue={user.firstName}/>
 							</FormGroup>
 							<FormGroup
 								label={t('user.lastname')}
-								labelFor="text-input"
+								labelFor="lastName"
 							>
-								<InputGroup leftIcon="person" id="text-input" disabled={true} value={user.lastName}/>
+								<InputGroup leftIcon="person" disabled={!isUserDataChangeEnabled} id="lastName" defaultValue={user.lastName}/>
 							</FormGroup>
 							<FormGroup
 								label={t('user.email')}
-								labelFor="text-input"
+								labelFor="email"
 							>
-								<InputGroup leftIcon="envelope" id="text-input" disabled={true} value={user.email}/>
+								<InputGroup
+									type="email"
+									onChange={onEmailInputChange}
+									leftIcon="envelope"
+									disabled={!isUserDataChangeEnabled}
+									id="email"
+									intent={isEmailValid ? Intent.SUCCESS : Intent.WARNING}
+									defaultValue={user.email}/>
 							</FormGroup>
 							<FormGroup
 								label={t('user.role')}
-								labelFor="select_role"
+								labelFor="role"
 							>
-								<div id="select-role" className="bp3-select bp3-fill">
-									<select disabled={true}>
-										<option selected={user.role === 'admin'} value="admin">Operator</option>
-										<option selected={user.role === 'pilot'} value="pilot">Pilot</option>
+								<div id="role" className="bp3-select bp3-fill">
+									<select value={user.role} disabled={true}>
+										<option value="admin">Operator</option>
+										<option value="pilot">Pilot</option>
 									</select>
 								</div>
 							</FormGroup>
-						</Card>
-						<Card className='fullHW' elevation={Elevation.TWO} style={{marginBottom: '5px'}}>
-							<FormGroup
-								label={t('user.change_password')}
-								labelFor="newpassword"
+							<Button
+								intent={Intent.PRIMARY}
+								disabled={!isUserDataChangeEnabled}
+								onClick={() => changeUserData()}
 							>
-								<InputGroup leftIcon="compass" id="newpassword"/>
-							</FormGroup>
+								Save changes
+							</Button>
+						</Card>
+						<Card className='fullHW' elevation={Elevation.TWO} style={{marginTop: '10px', marginBottom: '10px'}}>
+							{	!isPasswordChangeEnabled &&
+								<Button
+									intent={Intent.DANGER}
+									disabled={!isUserDataChangeEnabled}
+									onClick={() => setPasswordChangeEnabled(true)}
+								>
+									Change password
+								</Button>
+							}
+							{	isPasswordChangeEnabled &&
+								<>
+									<FormGroup
+										label={t('user.change_password')}
+										labelFor="newpassword"
+									>
+										<InputGroup type="password" leftIcon="compass" id="newpassword"/>
+									</FormGroup>
+									<Button
+										intent={Intent.PRIMARY}
+										onClick={() => changePassword()}
+									>
+									Save new password
+									</Button>
+								</>
+							}
 						</Card>
 					</>
 				}
