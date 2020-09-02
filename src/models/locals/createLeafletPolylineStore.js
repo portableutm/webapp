903389289e277eeecba8d9ea1828bca4a0ce7fc3
Mathias Betancurt <mathias@betancurt.dev>
2020-09-03 01:53:08 +0200

    feat: bumped version to 0.5.0-DEV due to major performance rework

---
+++ src/models/locals/createLeafletPolylineStore.js
@@ -0,0 +1,11 @@
+export function createLeafletPolylineStore() {
+	return {
+		leafletPolyline: null,
+		setPolyline(polyline) {
+			this.leafletPolyline = polyline;
+		},
+		get isPolylineInstanced() {
+			return this.leafletPolyline !== null;
+		}
+	};
+}
