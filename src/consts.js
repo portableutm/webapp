9b01e8c7374a926fea19de6a1bb7823fa30344f3
Mathias Betancurt <mathias@betancurt.dev>
2020-10-26 20:27:01 +0100

    chore: version code 1.0.0-DEV

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,6 @@
 let API;
 let DEBUG = false;
-const adesVersion = '0.5.2-DEV';
+const adesVersion = '1.0.0-DEV';
 const USERS_DATA_TOO_OLD = 2 * 60000; // Only fetch user list if saved user list is older than 2 minutes
 
 //const hostname = window && window.location && window.location.hostname;
