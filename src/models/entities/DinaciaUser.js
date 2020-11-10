947568e1f15ed04538228d8110b55eeb8e20fe04
Mathias Betancurt <mathias@betancurt.dev>
2020-11-10 20:39:18 +0100

    feat: create new user screen revamped

---
+++ src/models/entities/DinaciaUser.js
@@ -0,0 +1,41 @@
+import { types } from 'mobx-state-tree';
+
+export const BaseDinaciaCompany = types
+	// Used as a base class for vehicles. Mostly for data entry and publishing to the server.
+	.model('BaseDinaciaUser', {
+		razon_social : types.maybeNull(types.string),
+		nombre_comercial : types.maybeNull(types.string),
+		domicilio : types.maybeNull(types.string),
+		telefono : types.maybeNull(types.string),
+		RUT : types.maybeNull(types.string),
+	});
+
+export const DinaciaCompany = BaseDinaciaCompany
+	.named('DinaciaCompany')
+	.props({
+		id: types.string
+	});
+
+export const BaseDinaciaUser = types
+	// Used as a base class for vehicles. Mostly for data entry and publishing to the server.
+	.model('BaseDinaciaUser', {
+		address: types.maybeNull(types.string),
+		document_type: types.maybeNull(types.string),
+		document_number: types.maybeNull(types.string),
+		phone: types.maybeNull(types.string),
+		cellphone: types.maybeNull(types.string),
+		nationality: types.maybeNull(types.string),
+		dinacia_company: types.maybeNull(BaseDinaciaCompany)
+	})
+	.volatile(() => ({
+		document_file: null,
+		permit_front_file: null,
+		permit_back_file: null
+	}));
+
+export const DinaciaUser = BaseDinaciaUser
+	.named('DinaciaUser')
+	.props({
+		id: types.string,
+		dinacia_company: types.maybeNull(DinaciaCompany)
+	});
--- src/models/entities/User.js
+++ src/models/entities/User.js
@@ -1,21 +1,58 @@
 import { types } from 'mobx-state-tree';
+import { BaseDinaciaUser } from './DinaciaUser';
 
-export const User = types
-	.model('User', {
-		username: types.identifier,
+export const BaseUser = types
+	.model('BaseUser', {
+		username: types.string,
 		firstName: types.string,
 		lastName: types.string,
 		email: types.string,
 		password: types.maybe(types.string),
 		// TODO: We need it when creating a new User
 		role: types.enumeration('UserRoles', ['admin', 'pilot']),
+		dinacia_user: types.maybeNull(BaseDinaciaUser)
 		// volumesOfInterest:
 		// settings:
 	})
+	.actions((self) => ({
+		setProperty(prop, value) {
+			try {
+				self[prop] = value;
+				return true;
+			} catch (error) {
+				console.dir(error);
+				return false;
+			}
+		},
+		setDinaciaProperty(prop, value) {
+			try {
+				self.dinacia_user[prop] = value;
+				return true;
+			} catch (error) {
+				console.dir(error);
+				return false;
+			}
+		},
+		setDinaciaCompanyProperty(prop, value) {
+			try {
+				self.dinacia_user.dinacia_company[prop] = value;
+				return true;
+			} catch (error) {
+				console.dir(error);
+				return false;
+			}
+		}
+	}))
 	.views(self => {
 		return {
 			get asDisplayString() {
 				return `${self.firstName} ${self.lastName} (${self.username})`;
 			}
 		};
+	});
+
+export const User = BaseUser
+	.named('User')
+	.props({
+		username: types.identifier
 	});
